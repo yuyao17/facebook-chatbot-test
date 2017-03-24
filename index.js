10206c6c27f377ebd0192b131de4b2cedd502572
yuyao17 <yuya.oiwa@me.com>
2017-03-24 16:25:29 +0900

    init chatbot

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('express')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 3000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+var port = app.get("port")
+
+// Routes
+
+app.get('/', function(req, res){
+  res.send("Hi I am chatBot!!")
+})
+
+// Facebook
+
+app.get('/webook/', function(req, res){
+  if (req.query['hub.verify_token'] === "yuyaaaar") {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send("Wrong token")
+})
+
+// Listen to server
+
+app.listen(port, function(){
+  console.log(`listening to ${port}`)
+})
