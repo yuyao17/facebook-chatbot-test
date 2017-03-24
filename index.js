359131c6e900988410957b67917b5f25155628d1
yuyao17 <yuya.oiwa@me.com>
2017-03-24 17:59:57 +0900

    fix2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('express')
+const request = require('request')
 
 const app = express()
 
@@ -13,6 +13,7 @@ app.use(bodyParser.json())
 
 var port = app.get("port")
 
+var token = "EAAIlA8L7tAYBAOYsdb2UNTsh2kvCDCdEm8j1iaFvmAihoKZBGdHCeYXFPfKyyk1C1OaZBY6nUVKNg1m6ivbW1UfiNFkmjZAQ4xV2aXt0Tg3wFACYvfEyxKpyq1QNGJNi8bcZBgErERM2mYZAxKPao29kJR73PCb8r6YCutZBUoNAZDZD"
 // Routes
 
 app.get('/', function(req, res){
@@ -24,17 +25,41 @@ app.get('/', function(req, res){
 app.get('/webhook/', function(req, res){
   if (req.query['hub.verify_token'] === "yuyaaaar") {
     res.send(req.query['hub.challenge'])
-    res.json(req.query)
   }
   res.send("Wrong token")
 })
 
 app.post('/webhook/', function(req, res){
   let messaging_events = req.body.entry[0].messaging_events
+  console.log(messaging_events)
   for (let i = 0; i < messaging_events.length; i++) {
-
+    let event = messaging_events[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      sendText(sender, `text echo: ${text.substring(0, 100)}`)
+    }
   }
 })
+
+function sendText(sender, text) {
+  let messageData = {text: text}
+  request({
+    url:"https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token : token},
+    method: "POST",
+    json: {
+      receipt: {id: sender},
+      message: messageData
+    }
+  }, function(err, response, body){
+    if (err) {
+      console.log("Error sending message")
+    }, else if (response.body.error) {
+      console.log("response body error")
+    }
+  })
+}
 
 // Listen to server
 
